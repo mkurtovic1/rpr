7419532fb7b06bbfe964a8054e6ab8a251cbc836
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-18 13:24:00 +0200

    added KorisnikController.java and loginforma.fxml

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/KorisnikController.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class KorisnikController {
+    public Button btnlogin;
+    public TextField pass;
+    public TextField email;
+    public Button btncancel;
+
+    public void cancelAction(ActionEvent event) {
+    }
+
+    public void loginAction(ActionEvent event) {
+    }
+}
