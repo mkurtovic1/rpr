cfb16f5f7a41266d2abac4d617d27874803a5557
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 02:23:01 +0200

    created beforeeach test

--- src/test/java/ba/unsa/etf/rpr/bussines/KorisnikManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/bussines/KorisnikManagerTest.java
@@ -4,7 +4,11 @@ import ba.unsa.etf.rpr.business.KorisnikManager;
 import ba.unsa.etf.rpr.dao.KorisnikDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Korisnik;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class KorisnikManagerTest {
@@ -14,5 +18,18 @@ public class KorisnikManagerTest {
     private List<Korisnik> korisnici;
     //before each test
     @BeforeEach
+    public void initializeObjesWeNeed(){
+        korisnikManager= Mockito.mock(KorisnikManager.class);
+        korisnik=new Korisnik();
+        korisnik.setEmail("mkmk@gmail.com");
+        korisnik.setId(3);
+        korisnik.setLozinka("147258369");
+        korisnik.setIme("MK");
+        korisnik.setPrezime("k");
+
+        korisnikDaoSQL=Mockito.mock(KorisnikDaoSQLImpl.class);
+        korisnici=new ArrayList<>();
+        korisnici.addAll(Arrays.asList(new Korisnik("mirela", "kur", "253988", "df@gmail.com" )));
+    }
 
 }
