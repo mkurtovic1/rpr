a1786db938982e79e9fb2d5fd4e56f4d37137458
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 03:16:34 +0200

    created javadoc for class- dao

--- src/main/java/ba/unsa/etf/rpr/dao/IznajmljivanjeDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/IznajmljivanjeDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Iznajmljivanje;
-
+/**
+ * Dao interface for Iznajmljivanje domain bean
+ *
+ * @author MKurtovic
+ */
 public interface IznajmljivanjeDao extends Dao<Iznajmljivanje> {
 }
--- src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/KorisnikDao.java
@@ -1,6 +1,11 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Korisnik;
+/**
+ * Dao interface for Korisnik domain bean
+ *
+ * @author MKurtovic
+ */
 
 public interface KorisnikDao extends Dao<Korisnik> {
 }
