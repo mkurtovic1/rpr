f08ee81b58cf4648e82b4b3ae080cecb04ebb86b
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 02:59:11 +0200

    created javadoc for Vozilo.java

--- src/main/java/ba/unsa/etf/rpr/domain/Vozilo.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Vozilo.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.domain;
 
 import java.util.Objects;
-//samo da ga git pokupi
+/**
+ * List of possible categories for Vozilo
+ *
+ * @author MKurtovic
+ */
 public class Vozilo implements Idable{
     private int id;
     private String naziv;
