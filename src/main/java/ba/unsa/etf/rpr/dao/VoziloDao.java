83a95146b12ca086df860de24eac36e37582a08d
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 03:16:46 +0200

    created javadoc for class- dao

--- src/main/java/ba/unsa/etf/rpr/dao/VoziloDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/VoziloDao.java
@@ -3,7 +3,11 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Vozilo;
 
 import java.util.List;
-
+/**
+ * Dao interface for  Vozilo domain bean
+ *
+ * @author MKurtovic
+ */
 public interface VoziloDao extends Dao<Vozilo> {
     List<Vozilo> searchByText(String text) throws Exception;
 }
