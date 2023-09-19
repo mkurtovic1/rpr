8318689fc1cdece4ed9a610efa032e027e4ab7c9
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 02:11:29 +0200

    updated to jupiter api latest version

--- src/test/java/ba/unsa/etf/rpr/bussines/KorisnikManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/bussines/KorisnikManagerTest.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.bussines;
 import ba.unsa.etf.rpr.business.KorisnikManager;
 import ba.unsa.etf.rpr.dao.KorisnikDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Korisnik;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.List;
 
@@ -11,4 +12,7 @@ public class KorisnikManagerTest {
     private Korisnik korisnik;
     private KorisnikDaoSQLImpl korisnikDaoSQL;
     private List<Korisnik> korisnici;
+    //before each test
+    @BeforeEach
+
 }
