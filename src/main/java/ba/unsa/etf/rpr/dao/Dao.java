1c42bda84f3bc691466d53f5544d6a02cc842a9a
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 03:14:29 +0200

    created javadoc for dao

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 import java .util.List;
+/**
+ * Root interface for all DAO classes
+ *
+ * @author MKurtovic
+ */
 public interface Dao<T> {
     T getById(int id) throws Exception;
     T add(T item) throws Exception;
