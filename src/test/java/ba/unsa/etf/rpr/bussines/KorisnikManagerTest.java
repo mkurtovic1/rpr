52ee99b8aae5b3910f7e18c42e749c08de96d670
MKurtovic <mkurtovic1@etf.unsa.ba>
2023-09-19 02:09:35 +0200

    creating tests for KorisnikManager

---
+++ src/test/java/ba/unsa/etf/rpr/bussines/KorisnikManagerTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.bussines;
+
+import ba.unsa.etf.rpr.business.KorisnikManager;
+import ba.unsa.etf.rpr.dao.KorisnikDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Korisnik;
+
+import java.util.List;
+
+public class KorisnikManagerTest {
+    private KorisnikManager korisnikManager;
+    private Korisnik korisnik;
+    private KorisnikDaoSQLImpl korisnikDaoSQL;
+    private List<Korisnik> korisnici;
+}
